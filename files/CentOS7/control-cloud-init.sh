68a99d37e7784973a1db0c0172c65230cafae1e9
Cesar Jorge <cjmartinez@aubay.es>
2018-02-21 11:22:07 +0100

    task/enable_cloud-init_only_for_supported_virtual_environments_and_add_aws_master

--- files/CentOS7/control-cloud-init.sh
+++ files/CentOS7/control-cloud-init.sh
@@ -31,13 +31,24 @@ then
     sudo systemctl unmask ${cloudunit}
     sudo systemctl enable ${cloudunit}
   done
-else
+elif virt-what | grep -qs '^aws$'
+then
   sudo mkdir -p /etc/cloud
   sudo test -f /etc/cloud/cloud-init.disabled && sudo rm -f /etc/cloud/cloud-init.disabled || true
   for cloudunit in $(sudo systemctl list-unit-files | grep -s ^cloud- | cut -f1 -d' ')
   do
     sudo systemctl unmask ${cloudunit}
     sudo systemctl enable ${cloudunit}
+  done
+else
+  sudo rm -rf /var/log/cloud-init*.log
+  sudo mkdir -p /etc/cloud
+  sudo test -f /etc/cloud/cloud-init.disabled || sudo touch /etc/cloud/cloud-init.disabled
+  for cloudunit in $(sudo systemctl list-unit-files | grep -s ^cloud- | cut -f1 -d' ')
+  do
+    sudo systemctl stop ${cloudunit}
+    sudo systemctl disable ${cloudunit}
+    sudo systemctl mask ${cloudunit}
   done
 fi
 
