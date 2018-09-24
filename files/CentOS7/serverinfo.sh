c2bbe8e03530afdf9bdaec4c6e9c8dd2bc1501e9
Cesar Jorge <cjmartinez@aubay.es>
2018-09-24 11:54:31 +0200

    update host info

--- files/CentOS7/serverinfo.sh
+++ files/CentOS7/serverinfo.sh
@@ -13,20 +13,20 @@ RESET_COLORS="\e[0m"
 echo -e "===========================================================================
 ${COLOR_COLUMN}HOSTNAME${RESET_COLORS}...........: ${COLOR_VALUE}${MYHOSTNAME}${RESET_COLORS}
 ${COLOR_COLUMN}INTERFACES${RESET_COLORS}.........:
-${COLOR_VALUE}Interface\tMAC Address\t\tIP4 Address\t\tIP6 Address${RESET_COLORS}
-${COLOR_VALUE}$(for x in ${MYINTERFACES}
+${COLOR_VALUE}$(printf "%-18s%-18s%-46s%-46s" "Interface" "MAC Address" "IP4 Address" "IP6 Address" | sed -e 's/[[:space:]]*$//')${RESET_COLORS}
+${COLOR_VALUE}$(for ifacename in ${MYINTERFACES}
 do
-  mac=$(ip ad show dev $x | grep link/ether | awk '{print $2}')
-  ip4=$(ip ad show dev $x | grep -Fw inet | awk '{print $2}')
-  ip6=$(ip ad show dev $x | grep -Fw inet6 | awk '{print $2}')
-  printf  $x"\t\t"$mac"\t"$ip4"\t"$ip6"\n"
+  mac=$(ip ad show dev ${ifacename} | grep link/ether | awk '{print $2}')
+  ip4=$(ip ad show dev ${ifacename} | grep -Fw inet | awk '{print $2}')
+  ip6=$(ip ad show dev ${ifacename} | grep -Fw inet6 | awk '{print $2}')
+  printf "%-18s%-18s%-46s%-46s\n" "${ifacename}" "${mac}" "${ip4}" "${ip6}" | sed -e 's/[[:space:]]*$//'
 done)${RESET_COLORS}
 ${COLOR_COLUMN}CPU CORES${RESET_COLORS}..........: ${COLOR_VALUE}${MYCPUCORES}${RESET_COLORS}
 ${COLOR_COLUMN}MEMORY${RESET_COLORS}.............:
 ${COLOR_VALUE}$(free -h)${RESET_COLORS}
 ${COLOR_COLUMN}FILESYSTEMS${RESET_COLORS}........:
-${COLOR_VALUE}$(df -Ph)${RESET_COLORS}
-${COLOR_COLUMN}SYSTEM UPTIME${RESET_COLORS}......: ${COLOR_VALUE}$(uptime)${RESET_COLORS}
+${COLOR_VALUE}$(df -Ph | egrep -v '^overlay|^shm')${RESET_COLORS}
+${COLOR_COLUMN}SYSTEM UPTIME${RESET_COLORS}......: ${COLOR_VALUE}$(uptime | xargs)${RESET_COLORS}
 ${COLOR_COLUMN}RELEASE${RESET_COLORS}............: ${COLOR_VALUE}$(cat /etc/redhat-release)${RESET_COLORS}
 ${COLOR_COLUMN}KERNEL${RESET_COLORS}.............: ${COLOR_VALUE}$(uname -r)${RESET_COLORS}
 ${COLOR_COLUMN}DATE${RESET_COLORS}...............: ${COLOR_VALUE}$(date)${RESET_COLORS}
