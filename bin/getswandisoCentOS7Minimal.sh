c0b2c16452b52ec3e0375fbf6b369cb0dc2e69e7
Cesar Jorge <cesarjorgemartinez@gmail.com>
2019-09-25 16:00:44 +0200

    porting to use the new QEMU software

--- bin/getswandisoCentOS7Minimal.sh
+++ bin/getswandisoCentOS7Minimal.sh
@@ -31,6 +31,20 @@ do
 done
 chmod 755 *.exe
 
+QEMU_URL_DOWNLOAD="$(lynx -dump -listonly -nonumbers https://qemu.weilnetz.de/w64 | grep 'qemu-w64-setup-.*.exe' | sort -r | head -1)"
+echo "INFO: Get lastest QEMU Binaries for Windows (64 bit) <${QEMU_URL_DOWNLOAD}>"
+[[ "${QEMU_URL_DOWNLOAD}" == "" ]] && echo "ERROR: Cannot get lastest QEMU Binaries for Windows (64 bit)" && exit 1
+curl -L -O ${QEMU_URL_DOWNLOAD}
+echo "INFO: Please install QEMU Binaries for Windows (64 bit) using default options except for your language:
+  Please select a language: English / English
+  Click in OK
+  Next
+  I Agree
+  Next
+  Install
+  Finish"
+cygstart  --action=runas -w $(echo "${QEMU_URL_DOWNLOAD}" | sed 's#.*/##g')
+
 mkdir -p ${HOME_BASEDIR}/isos
 cd ${HOME_BASEDIR}/isos
 
