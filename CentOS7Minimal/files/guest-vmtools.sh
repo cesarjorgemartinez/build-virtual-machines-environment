5083c9c48425b5b6073b29e193075e56fa68b08b
Cesar Jorge <cesarjorgemartinez@gmail.com>
2020-05-31 19:11:26 +0200

    compatibilize guest-vmtools.sh

--- CentOS7Minimal/files/guest-vmtools.sh
+++ CentOS7Minimal/files/guest-vmtools.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+echo "INFO: Detects Operating System"
+SO_ID="$(source /etc/os-release && echo "${ID}")"
+SO_VERSION_ID="$(source /etc/os-release && echo "${VERSION_ID}")"
+echo "INFO: SO_ID <${SO_ID}>"
+echo "INFO: SO_VERSION_ID <${SO_VERSION_ID}>"
+
+PKG_MANAGER=
+if [ "${SO_ID}" == "centos" ]
+then
+  [[ "${SO_VERSION_ID}" == "7" ]] && PKG_MANAGER=yum
+  [[ "${SO_VERSION_ID}" == "8" ]] && PKG_MANAGER=dnf
+elif [ "${SO_ID}" == "ubuntu" ]
+then
+ export DEBIAN_FRONTEND=noninteractive
+else
+  echo "ERROR: Operating System type not supported"
+  exit 1
+fi
+
 echo "INFO: Detects if we are inside a virtual machine"
 MACHINETYPE="$(sudo virt-what)"
 if [ "$(echo "${MACHINETYPE}")" == "" ]
@@ -12,7 +31,8 @@ else
     echo "INFO: It is a VBOX virtual machine"
     echo "INFO: If exist the file /opt/reinstallGuestAdditions.action then force to reinstall the GuestAdditions"
     echo "INFO: Erase VMware packages"
-    sudo yum -y erase -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} erase -y -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get purge -y -qq open-vm-tools open-vm-tools-desktop
     echo "INFO: Get SMBIOS OEM Strings type 11 to find VBOX version of host"
     VBOXHOSTVERSION="$(LANG=C sudo dmidecode -q --type 11 2>&1 | sed -r -n -e 's/^.*vboxVer_(.+)$/\1/p')"
     echo "INFO: Get the VBOX guest version of guest if it is installed"
@@ -24,11 +44,20 @@ else
       echo "INFO: Install or update GuestAdditions"
       set +e
       sudo rm -rf /var/log/vboxadd*.log*
-      # Although disabling selinux is done in a previous step, it is reapplied in case some previous tasks enables it
-      sudo /sbin/setenforce 0 || true
-      sudo sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
-      sudo yum -y install -q bzip2 gcc make perl kernel-devel dkms
-      export KERN_DIR=/usr/src/kernels/$(uname -r)
+      if [ "${SO_ID}" == "centos" ]
+      then
+        # Although disabling selinux is done in a previous step, it is reapplied in case some previous tasks enables it
+        sudo /sbin/setenforce 0 || true
+        sudo sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
+        sudo ${PKG_MANAGER} install -y -q bzip2 gcc make perl kernel-devel dkms
+        export KERN_DIR=/usr/src/kernels/$(uname -r)
+      elif [ "${SO_ID}" == "ubuntu" ]
+      then
+        sudo apt-get install -y -qq bzip2 gcc make perl dkms
+      else
+        echo "ERROR: Operating System type not supported"
+        exit 1
+      fi
       sudo curl -s -S --fail --retry 3 --retry-delay 60 http://download.virtualbox.org/virtualbox/${VBOXHOSTVERSION}/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso -o /opt/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso
       sudo mkdir -p /mnt/VBoxGuestAdditionsISO
       sudo mountpoint -q /mnt/VBoxGuestAdditionsISO || sudo mount -o loop,ro /opt/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso /mnt/VBoxGuestAdditionsISO || true
@@ -47,7 +76,8 @@ else
     sudo /opt/VBoxGuestAdditions-*/uninstall.sh 2>/dev/null || true
     sudo rm -rf /opt/VBoxGuestAdditions* /var/log/vboxadd*.log*
     echo "INFO: Install VMware packages"
-    sudo yum -y install -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} install -y -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get install -y -qq open-vm-tools open-vm-tools-desktop
     sudo systemctl restart vmtoolsd.service
   elif [ "$(echo "${MACHINETYPE}" | grep '^kvm$')" != "" ]
   then
@@ -56,7 +86,8 @@ else
     sudo /opt/VBoxGuestAdditions-*/uninstall.sh 2>/dev/null || true
     sudo rm -rf /opt/VBoxGuestAdditions* /var/log/vboxadd*.log*
     echo "INFO: Erase VMware packages"
-    sudo yum -y erase -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} erase -y -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get purge -y -qq open-vm-tools open-vm-tools-desktop
   else
     echo "INFO: The virtual machine is not supported"
   fi
--- Ubuntu20Minimal/files/guest-vmtools.sh
+++ Ubuntu20Minimal/files/guest-vmtools.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-echo "INFO: Detects if we are inside a virtual machine"
+echo "INFO: Detects Operating System"
+SO_ID="$(source /etc/os-release && echo "${ID}")"
+SO_VERSION_ID="$(source /etc/os-release && echo "${VERSION_ID}")"
+echo "INFO: SO_ID <${SO_ID}>"
+echo "INFO: SO_VERSION_ID <${SO_VERSION_ID}>"
 
+PKG_MANAGER=
+if [ "${SO_ID}" == "centos" ]
+then
+  [[ "${SO_VERSION_ID}" == "7" ]] && PKG_MANAGER=yum
+  [[ "${SO_VERSION_ID}" == "8" ]] && PKG_MANAGER=dnf
+elif [ "${SO_ID}" == "ubuntu" ]
+then
+ export DEBIAN_FRONTEND=noninteractive
+else
+  echo "ERROR: Operating System type not supported"
+  exit 1
+fi
+
+echo "INFO: Detects if we are inside a virtual machine"
 MACHINETYPE="$(sudo virt-what)"
 if [ "$(echo "${MACHINETYPE}")" == "" ]
 then
@@ -13,7 +31,8 @@ else
     echo "INFO: It is a VBOX virtual machine"
     echo "INFO: If exist the file /opt/reinstallGuestAdditions.action then force to reinstall the GuestAdditions"
     echo "INFO: Erase VMware packages"
-    sudo dnf -y erase -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} erase -y -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get purge -y -qq open-vm-tools open-vm-tools-desktop
     echo "INFO: Get SMBIOS OEM Strings type 11 to find VBOX version of host"
     VBOXHOSTVERSION="$(LANG=C sudo dmidecode -q --type 11 2>&1 | sed -r -n -e 's/^.*vboxVer_(.+)$/\1/p')"
     echo "INFO: Get the VBOX guest version of guest if it is installed"
@@ -25,11 +44,20 @@ else
       echo "INFO: Install or update GuestAdditions"
       set +e
       sudo rm -rf /var/log/vboxadd*.log*
-      # Although disabling selinux is done in a previous step, it is reapplied in case some previous tasks enables it
-      sudo /sbin/setenforce 0 || true
-      sudo sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
-      sudo dnf -y install -q bzip2 gcc make perl kernel-devel dkms
-      export KERN_DIR=/usr/src/kernels/$(uname -r)
+      if [ "${SO_ID}" == "centos" ]
+      then
+        # Although disabling selinux is done in a previous step, it is reapplied in case some previous tasks enables it
+        sudo /sbin/setenforce 0 || true
+        sudo sed -i s/SELINUX=enforcing/SELINUX=disabled/g /etc/selinux/config
+        sudo ${PKG_MANAGER} install -y -q bzip2 gcc make perl kernel-devel dkms
+        export KERN_DIR=/usr/src/kernels/$(uname -r)
+      elif [ "${SO_ID}" == "ubuntu" ]
+      then
+        sudo apt-get install -y -qq bzip2 gcc make perl dkms
+      else
+        echo "ERROR: Operating System type not supported"
+        exit 1
+      fi
       sudo curl -s -S --fail --retry 3 --retry-delay 60 http://download.virtualbox.org/virtualbox/${VBOXHOSTVERSION}/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso -o /opt/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso
       sudo mkdir -p /mnt/VBoxGuestAdditionsISO
       sudo mountpoint -q /mnt/VBoxGuestAdditionsISO || sudo mount -o loop,ro /opt/VBoxGuestAdditions_${VBOXHOSTVERSION}.iso /mnt/VBoxGuestAdditionsISO || true
@@ -48,7 +76,8 @@ else
     sudo /opt/VBoxGuestAdditions-*/uninstall.sh 2>/dev/null || true
     sudo rm -rf /opt/VBoxGuestAdditions* /var/log/vboxadd*.log*
     echo "INFO: Install VMware packages"
-    sudo dnf -y install -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} install -y -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get install -y -qq open-vm-tools open-vm-tools-desktop
     sudo systemctl restart vmtoolsd.service
   elif [ "$(echo "${MACHINETYPE}" | grep '^kvm$')" != "" ]
   then
@@ -57,7 +86,8 @@ else
     sudo /opt/VBoxGuestAdditions-*/uninstall.sh 2>/dev/null || true
     sudo rm -rf /opt/VBoxGuestAdditions* /var/log/vboxadd*.log*
     echo "INFO: Erase VMware packages"
-    sudo dnf -y erase -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "centos" ]] && sudo ${PKG_MANAGER} erase -y -C -q open-vm-tools open-vm-tools-desktop
+    [[ "${SO_ID}" == "ubuntu" ]] && sudo apt-get purge -y -qq open-vm-tools open-vm-tools-desktop
   else
     echo "INFO: The virtual machine is not supported"
   fi
